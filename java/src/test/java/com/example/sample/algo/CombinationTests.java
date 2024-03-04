e2f898130e2e7b12f62c103b8c6bf768fc456f1d
haeram27 <haeram27@gmail.com>
2024-03-04 16:14:06 +0900

    add permutation

--- java/src/test/java/com/example/sample/algo/CombinationTests.java
+++ java/src/test/java/com/example/sample/algo/CombinationTests.java
@@ -15,11 +15,11 @@ public class CombinationTests {
      * @param data      element list (size >= r)
      * @param out       result of combination (size = r)
      * @param r         size of out
-     * @param start     start index in data to be candidate
+     * @param start     start is index of data to be candidate of out[depth]
      * @param depth     current level of combination, if depth==2 then 0~(depth-1) index is filled in out array 
      */
-    public <E> void combination(E[] data, E[] out, int r, int start, int depth) {
-        System.out.println(String.format("depth=%d, start=%d", depth, start));
+    public <T> void combination(T[] data, T[] out, int r, int depth, int start) {
+        //System.out.println(String.format("depth=%d, start=%d", depth, start));
         if (depth == r) {
             for (var e : out) {
                 System.out.print(e + " ");
@@ -30,15 +30,15 @@ public class CombinationTests {
 
         for (int i = start; i < data.length; i++) {
             out[depth] = data[i];
-            combination(data, out, r, i + 1, depth + 1);
+            combination(data, out, r, depth + 1, i + 1);
         }
     }
 
     @Test
     public void run() {
         int r = 2;
-        Character[] data = {'a', 'b', 'c', 'd'}; // n = 4
-        Character[] out = new Character[r];
+        String[] data = {"a", "b", "c", "d"}; // n = 4
+        String[] out = new String[r];
 
         System.out.println("\n[combination recursive]===================");
 
--- java/src/test/java/com/example/sample/algo/PermutationTests.java
+++ java/src/test/java/com/example/sample/algo/PermutationTests.java
@@ -1,12 +1,54 @@
 package com.example.sample.algo;
 
+import java.util.Arrays;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 public class PermutationTests {
+    /**
+     * TODO: implement permutation() Under Here
+     *
+     * @brief permutation
+     *
+     * @tparam T
+     * @param data      element list (size >= r)
+     * @param out       result of combination (size = r)
+     * @param visited   check using element of data while traversal
+     * @param depth     index of out(permutation), if depth==2 then 0~(depth-1) index is filled in out array
+     *                  index i on for statement is index of data to be candidate of out[depth]
+     */
+    public <T> void permutation(T[] data, T[] out, boolean[] visited, int r, int depth) {
+        //System.out.println(String.format("depth=%d, start=%d", depth, start));
+        if (depth == r) {
+            for (var e : out) {
+                System.out.print(e + " ");
+            }
+            System.out.println();
+            return;
+        }
+
+        for (int i = 0; i < data.length; i++) {
+            if (visited[i] != true) {
+                visited[i] = true;
+                out[depth] = data[i];
+                permutation(data, out, visited, r, depth + 1);
+                visited[i] = false;
+            }
+        }
+    }
+
     @Test
     public void run() {
-        System.out.println("hello");
+        int r = 2;
+        String[] data = {"a", "b", "c", "d"}; // n = 4
+        String[] out = new String[r];
+        boolean[] visited = new boolean[data.length];
+        Arrays.fill(visited, false);
+
+        System.out.println("\n[permutation recursive]===================");
+
+        /* !!!! combination caller !!!! */
+        permutation(data, out, visited, r, 0);
     }
 }
