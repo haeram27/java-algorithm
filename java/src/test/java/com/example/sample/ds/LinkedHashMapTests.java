0796d9ad314b5753ad4e842774f913700b04273b
haeram27 <haeram27@gmail.com>
2024-03-06 15:25:50 +0900

    update map test

--- java/src/test/java/com/example/sample/ds/LinkedHashMapTests.java
+++ java/src/test/java/com/example/sample/ds/LinkedHashMapTests.java
@@ -1,25 +1,64 @@
 package com.example.sample.ds;
 
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.stream.Collectors;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+
 @SpringBootTest
 public class LinkedHashMapTests {
+
     @Test
     public void run() {
-        LinkedHashMap<String, String> lhs = new LinkedHashMap<>();
-        lhs.put("1", "a");
-        lhs.put("2", "b");
-        lhs.entrySet().forEach(e -> {
+        LinkedHashMap<String, String> map = new LinkedHashMap<>();
+        map.put("1", "a");
+        map.put("2", "b");
+
+        map.forEach((k, v) -> {
+            System.out.println("k::" + k);
+            System.out.println("v::" + v);
+        });
+        System.out.println("-".repeat(10));
+
+        map.entrySet().forEach(e -> {
             System.out.println("k::" + e.getKey());
             System.out.println("v::" + e.getValue());
         });
+        System.out.println("-".repeat(10));
 
-        lhs.forEach((k, v) -> {
-            System.out.println("k::" + k);
-            System.out.println("k::" + v);
+        map.entrySet().stream().sorted(Collections.reverseOrder(Map.Entry.comparingByValue()))
+                .forEach(e -> {
+                    System.out.println("k::" + e.getKey());
+                    System.out.println("v::" + e.getValue());
+                });
+        System.out.println("-".repeat(10));
+
+        map.entrySet().stream().sorted((o1, o2) -> o2.getValue().compareTo(o1.getValue()))
+                .forEach(e -> {
+                    System.out.println("k::" + e.getKey());
+                    System.out.println("v::" + e.getValue());
+                });
+        System.out.println("-".repeat(10));
+
+        map.entrySet().stream().sorted((o1, o2) -> o2.getValue().compareTo(o1.getValue()))
+                .collect(Collectors.toList()).forEach(e -> {
+                    System.out.println("k::" + e.getKey());
+                    System.out.println("v::" + e.getValue());
+                });
+        System.out.println("-".repeat(10));
+
+        var list = new ArrayList<Map.Entry<String, String>>();
+        for (var e : map.entrySet()) {
+            list.add(e);
+        }
+        list.forEach(e -> {
+            System.out.println("k::" + e.getKey());
+            System.out.println("v::" + e.getValue());
         });
-
+        System.out.println("-".repeat(10));
     }
 }
