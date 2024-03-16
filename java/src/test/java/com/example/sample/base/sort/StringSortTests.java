bca0e0d9014d6136dc002501836604d17900eea7
haeram27 <haeram27@gmail.com>
2024-03-16 16:45:33 +0900

    add String sort test

---
+++ java/src/test/java/com/example/sample/base/sort/StringSortTests.java
@@ -0,0 +1,60 @@
+package com.example.sample.base.sort;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class StringSortTests {
+    @Test
+    public void CodePointStreamSort() {
+        String sorted;
+
+        sorted = s.chars().mapToObj(Character::toString).sorted(Collections.reverseOrder())
+                .collect(Collectors.joining());
+        System.out.println(sorted);
+
+        // IntStream.sorted() can NOT use Collections.reverseOrder
+        sorted = s.chars().sorted().collect(StringBuilder::new, StringBuilder::appendCodePoint, StringBuilder::append)
+                .toString();
+        System.out.println(sorted);
+    }
+
+    @Test
+    public void StringStreamSort() {
+        String sorted;
+        sorted = Stream.of(s.split("")).sorted(Collections.reverseOrder()).collect(Collectors.joining());
+        System.out.println(sorted);
+    }
+
+    String s = "abcdeghijklmn";
+
+    <T> void swap(int i, int j, T[] a) {
+        T tmp = a[i];
+        a[i] = a[j];
+        a[j] = tmp;
+    }
+
+    @Test
+    public void CharArrayAscendingSort() {
+        var ar = s.toCharArray();
+        Arrays.sort(ar);
+        System.out.println(new String(ar));
+    }
+
+    @Test
+    public void CharArrayDescendingSort() {
+        var ar = s.toCharArray();
+        Arrays.sort(ar);
+        for (int i = 0, j = ar.length - 1; i < j; i++, j--) {
+            char tmp = ar[i];
+            ar[i] = ar[j];
+            ar[j] = tmp;
+        }
+
+        System.out.println(new String(ar));
+    }
+}
