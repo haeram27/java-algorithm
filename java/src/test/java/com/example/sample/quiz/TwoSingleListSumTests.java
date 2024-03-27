3d5bec158ce7a7c6473e93434e833822e925c8c5
haeram27 <haeram27@gmail.com>
2024-03-27 13:12:52 +0900

    add TwoSingleListTests.java

--- java/src/test/java/com/example/sample/quiz/TwoSingleListSumTests.java
+++ java/src/test/java/com/example/sample/quiz/TwoSingleListSumTests.java
@@ -16,55 +16,23 @@ class Node {
 
 public class TwoSingleListSumTests {
 
+    void printNode(Node n) {
+        while (n != null) {
+            System.out.print(n.data + " ");
+            n = n.next;
+        }
+        System.out.println();
+    }
+
     Node nodeSumTest(Node l1, Node l2) {
         // TODO:
-        Node n = new Node();
-        Node head = n;
+        Node head = null;
 
-        int p = 0, r = 0;
-        int l1v, l2v;
-
-        while (l1 != null || l2 != null) {
-            if (l1 != null) {
-                l1v = l1.data;
-            } else {
-                l1v = 0;
-            }
-
-            if (l2 != null) {
-                l2v = l2.data;
-            } else {
-                l2v = 0;
-            }
-
-            int v = l1v + l2v + p;
-            p = v / 10;
-            r = v % 10;
-
-            n.data = r;
-            n.next = new Node();
-            n = n.next;
-
-            if (l1 != null) {
-                l1 = l1.next;
-            }
-
-            if (l2 != null) {
-                l2 = l2.next;
-            }
-        }
         return head;
     }
 
-    void printNode(Node n) {
-        while (n != null) {
-            System.out.println(n.data);
-            n = n.next;
-        }
-    }
-
     @Test
-    void runA() {
+    void run1() {
         Node a, b, c;
         Node l1, l2;
 
@@ -84,12 +52,12 @@ public class TwoSingleListSumTests {
         l2 = a;
         printNode(l2);
 
-        Node n = nodeSumTest(l1, l2);
+        Node n = nodeSumTestA(l1, l2);
         printNode(n);
     }
 
     @Test
-    void runB() {
+    void run2() {
         Node a, b, c;
         Node l1, l2;
 
@@ -109,7 +77,99 @@ public class TwoSingleListSumTests {
         l2 = a;
         printNode(l2);
 
-        Node n = nodeSumTest(l1, l2);
+        Node n = nodeSumTestA(l1, l2);
+        printNode(n);
+    }
+
+    Node nodeSumTestA(Node l1, Node l2) {
+        // ANSWER:
+        Node head = null, last = null;
+        int q = 0; // quotient
+
+        while (l1 != null || l2 != null) {
+            int v1 = 0, v2 = 0;
+            if (l1 == null) {
+                v1 = 0;
+            } else {
+                v1 = l1.data;
+            }
+
+            if (l2 == null) {
+                v2 = 0;
+            } else {
+                v2 = l2.data;
+            }
+
+            int v = v1 + v2 + q;
+            q = v / 10;
+            int r = v % 10;
+
+            if (head == null) {
+                head = new Node(r);
+                last = head;
+            } else {
+                last.next = new Node(r);
+                last = last.next;
+            }
+
+            if (l1 != null) {
+                l1 = l1.next;
+            }
+
+            if (l2 != null) {
+                l2 = l2.next;
+            }
+        }
+        return head;
+    }
+
+    @Test
+    void runA1() {
+        Node a, b, c;
+        Node l1, l2;
+
+        a = new Node(1);
+        b = new Node(2);
+        c = new Node(3);
+        a.next = b;
+        b.next = c;
+        l1 = a;
+        printNode(l1);
+
+        a = new Node(4);
+        b = new Node(5);
+        c = new Node(6);
+        a.next = b;
+        b.next = c;
+        l2 = a;
+        printNode(l2);
+
+        Node n = nodeSumTestA(l1, l2);
+        printNode(n);
+    }
+
+    @Test
+    void runA2() {
+        Node a, b, c;
+        Node l1, l2;
+
+        a = new Node(9);
+        b = new Node(9);
+        c = new Node(9);
+        a.next = b;
+        b.next = c;
+        l1 = a;
+        printNode(l1);
+
+        a = new Node(9);
+        b = new Node(9);
+        c = new Node(0);
+        a.next = b;
+        b.next = c;
+        l2 = a;
+        printNode(l2);
+
+        Node n = nodeSumTestA(l1, l2);
         printNode(n);
     }
 
